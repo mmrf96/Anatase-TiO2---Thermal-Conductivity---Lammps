5f1bf2ac634f632e52672d671a403452014466d4
mmrf96 <rafieemm96@gmail.com>
2025-09-05 01:53:18 -0400

    Delete Backup directory

--- Backup/Cylind2Cartez.m
+++
@@ -1,5 +0,0 @@
-function [XX,YY,ZZ] = Cylind2Cartez(RR,Theta,zz)
-    XX = RR * cos(Theta);
-    YY = RR * sin(Theta);
-    ZZ = zz;
-end
--- Backup/Line2Circle.m
+++
@@ -1,4 +0,0 @@
-function [rr,tt] = Line2Circle(xx,yy,Radius,Lx,Thickness)
-    rr = Radius + (yy-Thickness/2);
-    tt = (xx / Lx) * 2*pi;
-end
